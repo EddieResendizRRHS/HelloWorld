e7bf0f0bc70c9916b744801eefc3eefe826b1938
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-10-21 05:58:12 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		// Display message Hello World! to the console
+		System.out.println("");
+	}
+}
---
+++ Message.java
@@ -0,0 +1,10 @@
+public class Message
+{
+	public static void main(String[] args)
+	{
+    // Display three messages
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
