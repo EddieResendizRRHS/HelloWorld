5c13aaf16ac5e8d11db68b9c9c817c26bed360c4
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-10-23 10:43:22 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		// Display message Hello World! to the console
-		System.out.println("");
+		System.out.println("Say Hello");
 	}
 }
