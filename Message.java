997729718689d4940e92a0d259d3388928e1bbb9
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-10-23 10:43:53 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -2,9 +2,9 @@ public class Message
 {
 	public static void main(String[] args)
 	{
-    // Display three messages
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+    		// Display three messages
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 	}
 }
